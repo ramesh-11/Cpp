a337eb6d641e3a1300c5f96372acf7d52eb594ec
Ramesh <59366319+ramesh-11@users.noreply.github.com>
2021-02-14 09:41:53 +0530

    Update 10.NewDelete.cpp

--- Programming Cpp/10.NewDelete.cpp
+++ Programming Cpp/10.NewDelete.cpp
@@ -1,48 +1,48 @@
-//#include <iostream>
-//#include<stdlib.h>
-//using namespace std;
-//class Test{
-//	int a;
-//	int b;
-//	public:
-//		Test(int=0,int=0);
-//		void* operator new(size_t);
-//		void operator delete(void *);
-//		void operator delete[](void *);
-//		void* operator new[](size_t);		
-//		~Test();
-//};
-//Test ::Test(int x,int y){
-//	cout<<endl<<"constructor called..";
-//	a=x;
-//	b=y;
-//}
-//void Test::operator delete(void *pt){
-//	cout<<endl<<"operator delete overload..";
-//	free (pt);
-//}
-//void Test::operator delete[](void *pt){
-//	cout<<endl<<"operator delete[] overload..";
-//	free (pt);
-//}
-//void* Test::operator new(size_t v){   	//v=size of Test =8 byte
-//	cout<<endl<<"operator new overload.. "<<v;
-//	void* tmp=malloc(v);
-//	return tmp;
-//}
-//
-//void* Test::operator new[](size_t v){   	//v=size of Test =32 byte
-//	cout<<endl<<"operator new[] overload.. "<<v;
-//	void* tmp=malloc(v);
-//	return tmp;
-//}
-//Test::~Test(){
-//	cout<<endl<<"destructor call..";
-//}
-//int main(){
-//	Test *ob1=new Test;  	// operator new
-//	delete ob1;
-//	Test *ob2=new Test[2];		// operator new[]
-//	delete [] ob2;
-//	return 0;
-//}
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+class Test{
+	int a;
+	int b;
+	public:
+		Test(int=0,int=0);
+		void* operator new(size_t);
+		void operator delete(void *);
+		void operator delete[](void *);
+		void* operator new[](size_t);		
+		~Test();
+};
+Test ::Test(int x,int y){
+	cout<<endl<<"constructor called..";
+	a=x;
+	b=y;
+}
+void Test::operator delete(void *pt){
+	cout<<endl<<"operator delete overload..";
+	free (pt);
+}
+void Test::operator delete[](void *pt){
+	cout<<endl<<"operator delete[] overload..";
+	free (pt);
+}
+void* Test::operator new(size_t v){   	//v=size of Test =8 byte
+	cout<<endl<<"operator new overload.. "<<v;
+	void* tmp=malloc(v);
+	return tmp;
+}
+
+void* Test::operator new[](size_t v){   	//v=size of Test =32 byte
+	cout<<endl<<"operator new[] overload.. "<<v;
+	void* tmp=malloc(v);
+	return tmp;
+}
+Test::~Test(){
+	cout<<endl<<"destructor call..";
+}
+int main(){
+	Test *ob1=new Test;  	// operator new
+	delete ob1;
+	Test *ob2=new Test[2];		// operator new[]
+	delete [] ob2;
+	return 0;
+}
